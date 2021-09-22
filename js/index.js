96e5bc539288087d01dd52849e0b0081a1ab2b6f
deleiteEmpanadas <deleite.empanadas2012@gmail.com>
2021-09-21 22:30:21 -0500

    tercer commit

---
+++ js/index.js
@@ -0,0 +1,109 @@
+window.sr = ScrollReveal();
+
+	sr.reveal('.fondo', {
+		duration: 3000, //tiempo
+		origin: 'bottom', //origen
+		distance: '-50px', //distancia
+		
+
+
+	}); 
+
+	sr.reveal('.buttons', {
+		duration: 3000, //tiempo
+		origin: 'bottom', //origen
+		distance: '-100px', //distancia
+		
+
+	}); 
+
+	sr.reveal('.imagen1', {
+		duration: 3000, //tiempo
+		origin: 'right', //origen
+		distance: '-100px', //distancia
+		reset: 1000
+
+	});
+
+	sr.reveal('.cajeta1', {
+		duration: 3000, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+
+	sr.reveal('.piña1', {
+		duration: 3000, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+
+sr.reveal('.fresa1', {
+		duration: 3000, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+
+sr.reveal('.crema1', {
+		duration: 3000, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+
+sr.reveal('.nutella1', {
+		duration: 3000, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+
+sr.reveal('.zarzamora1', {
+		duration: 3000, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+
+sr.reveal('.combinada21', {
+		duration: 3000, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+
+sr.reveal('.combinada31', {
+		duration: 3000, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+
+sr.reveal('.surtida1', {
+		duration: 3000, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+
+
+	
+
+	sr.reveal('.piepagina', {
+		duration: 2000, //tiempo
+		origin: 'bottom', //origen
+		distance: '-100px', //distancia
+		reset:100
+
+	}); 
