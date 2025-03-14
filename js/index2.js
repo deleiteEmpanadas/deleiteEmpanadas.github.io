230a8f6acf2178f4cb2a386419be21f5111c1108
deleiteEmpanadas <90789230+deleiteEmpanadas@users.noreply.github.com>
2025-03-13 18:29:23 -0600

    Add files via upload

--- js/index2.js
+++ js/index2.js
@@ -12,13 +12,36 @@ window.sr = ScrollReveal();
 		
 
 	});
+sr.reveal('.prox2', {
+		duration: 2500, //tiempo
+		origin: 'top', //origen
+		distance: '-300px', //distancia
+		delay:1000
+		
 
+	}); 
 
 	sr.reveal('.cuerpo', {
 		duration: 2500, //tiempo
 		origin: 'top', //origen
 		distance: '-300px', //distancia
 		delay:1700
+		
+
+	}); 
+	sr.reveal('.titulo2', {
+		duration: 2500, //tiempo
+		origin: 'left', //origen
+		distance: '200px', //distancia
+		delay:1000
+		
+
+	}); 
+	sr.reveal('.btn', {
+		duration: 2500, //tiempo
+		origin: 'bottom', //origen
+		distance: '200px', //distancia
+		delay:1400
 		
 
 	}); 
@@ -38,6 +61,13 @@ window.sr = ScrollReveal();
 		
 
 	}); 
+	sr.reveal('.destinos3', {
+		duration: 4500, //tiempo
+		delay:1000,
+		origin: 'top', //origen
+		distance: '-400px', //distancia
+
+	});
 	sr.reveal('.empanada', {
 		duration: 3000, //tiempo
 		origin: 'left', //origen
